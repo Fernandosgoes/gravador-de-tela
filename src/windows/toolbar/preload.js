09a67c0bc5c2c496c0fbe272b4f9a2cf9158352f
Fernando <fernandogoex@gmail.com>
2026-07-26 12:46:22 -0300

    feat: expose countdown, open-folder, resize, and settings-update bridge methods

--- src/windows/toolbar/preload.js
+++ src/windows/toolbar/preload.js
@@ -4,8 +4,12 @@ contextBridge.exposeInMainWorld('gravador', {
   listSources: () => ipcRenderer.invoke('capture:list-sources'),
   pickArea: () => ipcRenderer.invoke('areaselect:pick'),
   setOverlayTool: (payload) => ipcRenderer.send('overlay:set-tool', payload),
-  openSettings: () => ipcRenderer.send('open-settings'),
   getSettings: () => ipcRenderer.invoke('settings:get'),
+  updateSettings: (settings) => ipcRenderer.send('settings:update', settings),
+  onSettingsChanged: (callback) => ipcRenderer.on('settings:changed', (event, settings) => callback(settings)),
   saveRecording: (arrayBuffer) => ipcRenderer.invoke('export:save', arrayBuffer),
-  notifyState: (state) => ipcRenderer.send('recording:state-changed', state)
+  notifyState: (state) => ipcRenderer.send('recording:state-changed', state),
+  runCountdown: () => ipcRenderer.invoke('countdown:run'),
+  openLastFolder: () => ipcRenderer.invoke('export:open-last-folder'),
+  resizeWindow: (size) => ipcRenderer.send('toolbar:resize', size)
 });
